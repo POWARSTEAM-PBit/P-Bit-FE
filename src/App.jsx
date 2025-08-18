18bccd6fa111641889ede6a7a714dc86019071ca
rmit-s3460129-anssar-almashama <rmit-s3460129-anssar-almashama@users.noreply.github.com>
2025-08-18 19:31:23 +1000

    resolving merge 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import StudentLoginPage from './pages/StudentLoginPage';
@@ -15,4 +16,26 @@ export default function App() {
       </Routes>
     </>
   );
+=======
+// import React from 'react';
+// import Header from "./components/Header.jsx";
+
+// export default function App() {
+//     return (
+//         <>
+//             <Header />
+//             <div style={{ padding: '2rem' }}>
+//                 <h1>Welcome to MyApp</h1>
+//                 <p>Hi! This is your starting page content.</p>
+//             </div>
+//         </>
+//     );
+// }
+
+
+import AppRoutes from "./routes/AppRoutes";
+
+export default function App() {
+  return <AppRoutes />;
+>>>>>>> Stashed changes
 }
